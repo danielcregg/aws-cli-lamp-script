c8b25194bb916172b927e7b74ab60597a658a53f
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-03-10 10:23:30 +0000

    Update awsLamp.sh

--- awsLamp.sh
+++ awsLamp.sh
@@ -53,13 +53,6 @@ sudo chmod 600 id_rsa
 sudo mkdir -p ~/.ssh && sudo mv id_rsa ~/.ssh/
 
 echo Finding the latest Amazon Linux 2 AMI in the current region...
-#AMI_ID=$(aws ec2 describe-images \
-#  --owners amazon \
-#  --filters 'Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2' 'Name=state,Values=available' \
-#  --query 'sort_by(Images, &CreationDate)[-1].ImageId' \ 
-#  --output text)
-
-echo Finding the latest Amazon Linux 2 AMI in the current region...
 AMI_ID=$(aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-*-x86_64-gp2' 'Name=state,Values=available' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 
 INSTANCE_ID=$(aws ec2 run-instances \
