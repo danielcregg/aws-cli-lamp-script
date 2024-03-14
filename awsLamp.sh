cb499ff994eb75622fd0104e5c50e956776f2670
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-03-14 11:47:56 +0000

    Update awsLamp.sh

--- awsLamp.sh
+++ awsLamp.sh
@@ -211,7 +211,8 @@ sudo apt -y install unzip php-dom php-xml php-mbstring &&
 sudo service apache2 restart &&
 sudo wget https://builds.matomo.org/matomo.zip -P /var/www/html/ &&
 sudo unzip -oq /var/www/html/matomo.zip -d /var/www/html/ &&
-sudo rm -rf /var/www/html/matomo.zip "How to install Matomo.html" &&
+sudo rm -rf /var/www/html/matomo.zip &&
+sudo rm -rf /var/www/html/'How to install Matomo.html' &&
 sudo chown -R www-data:www-data /var/www/html/matomo;sudo chmod -R 0755 /var/www/html/matomo/tmp &&
 sudo mysql -Bse "CREATE DATABASE matomodb;CREATE USER matomoadmin@localhost IDENTIFIED BY \"password\";GRANT ALL PRIVILEGES ON matomodb.* TO matomoadmin@localhost; FLUSH PRIVILEGES;" &&
 sudo -u www-data wp plugin install matomo --activate &&
